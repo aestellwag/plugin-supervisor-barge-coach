ee6aac7a30ba5522d7a521aee396ec9f820e6822
Andrew Stellwag <aestellwag@gmail.com>
2021-09-24 15:17:41 -0400

    Updated Panel Components

--- plugin-supervisor-barge-coach/src/components/CoachingStatusPanel.js
+++ plugin-supervisor-barge-coach/src/components/CoachingStatusPanel.js
@@ -24,33 +24,52 @@ const Status = styled('div')`
 
 class CoachingStatusPanel extends React.Component {
 
-  render() {
-    const myWorkerSID = this.props.myWorkerSID;
-    let supervisorArray = this.props.supervisorArray;   
-    // Let's subscribe to the sync doc as an agent/work and check
-    // if we are being coached, if we are, render that in the UI
-    // otherwise leave it blank
-    const mySyncDoc = `syncDoc.${myWorkerSID}`;
-    SyncDoc.getSyncDoc(mySyncDoc)
-    .then(doc => {
-      // We are subscribing to Sync Doc updates here and logging anytime that happens
-      doc.on("updated", updatedDoc => {
-        if (doc.value.data.supervisors != null) {
-          supervisorArray = [...doc.value.data.supervisors];
-        } else {
-          supervisorArray = [];
-        }
+  // Use to validate if we have subscribed to sync updates or not
+  #syncSubscribed = false;
 
-        // Set Supervisor's name that is coaching into props
-        this.props.setBargeCoachStatus({ 
-          supervisorArray: supervisorArray
-        });
-      })
-    });
+  syncUpdates() {
+
+    //FIXME: Remove - this is for testing only
+    console.error(`Within Coaching Status Panel syncSubscribed = ${this.#syncSubscribed}`);
+
+    if (this.#syncSubscribed != true) {
+      
+      const myWorkerSID = this.props.myWorkerSID;
+      let supervisorArray = this.props.supervisorArray;   
+      // Let's subscribe to the sync doc as an agent/work and check
+      // if we are being coached, if we are, render that in the UI
+      // otherwise leave it blank
+      const mySyncDoc = `syncDoc.${myWorkerSID}`;
+      SyncDoc.getSyncDoc(mySyncDoc)
+      .then(doc => {
+        // We are subscribing to Sync Doc updates here and logging anytime that happens
+        doc.on("updated", updatedDoc => {
+          if (doc.value.data.supervisors != null) {
+            supervisorArray = [...doc.value.data.supervisors];
+          } else {
+            supervisorArray = [];
+          }
+  
+          // Set Supervisor's name that is coaching into props
+          this.props.setBargeCoachStatus({ 
+            supervisorArray: supervisorArray
+          });
+        })
+      });
+      this.#syncSubscribed = true;
+    }
+
+    return;
+  }
+  render() {
+    
+    const { supervisorArray } = this.props;
+    this.syncUpdates();
+
     // If the supervisor array has value in it, that means someone is coaching
     // We will map each of the supervisors that may be actively coaching
     // Otherwise we will not display anything if no one is actively coaching
-    if (this.props.supervisorArray.length != 0) {
+    if (supervisorArray.length != 0) {
       return (
         <Status>
           <div>You are being Coached by: 
--- plugin-supervisor-barge-coach/src/components/SupervisorMonitorPanel.js
+++ plugin-supervisor-barge-coach/src/components/SupervisorMonitorPanel.js
@@ -23,6 +23,9 @@ const Status = styled('div')`
 
 class SupervisorMonitorPanel extends React.Component {
 
+  // Use to validate if we have subscribed to sync updates or not
+  #syncSubscribed = false;
+
   supervisorArray() {
     
     return (
@@ -35,32 +38,46 @@ class SupervisorMonitorPanel extends React.Component {
     )
  }
 
-  render() {
-    // If the supervisor array has value in it, that means someone is coaching
-    // We will map each of the supervisors that may be actively coaching
-    // Otherwise we will not display anything if no one is actively coaching
-    const agentWorkerSID = this.props.agentWorkerSID;
-    let supervisorArray = this.props.supervisorArray;   
-    // Let's subscribe to the sync doc as an agent/work and check
-    // if we are being coached, if we are, render that in the UI
-    // otherwise leave it blank
-    const mySyncDoc = `syncDoc.${agentWorkerSID}`;
-    SyncDoc.getSyncDoc(mySyncDoc)
-    .then(doc => {
-      // We are subscribing to Sync Doc updates here and logging anytime that happens
-      doc.on("updated", updatedDoc => {
-        if (doc.value.data.supervisors != null) {
-          supervisorArray = [...doc.value.data.supervisors];
-        } else {
-          supervisorArray = [];
-        }
+ syncUpdates() {
+    //FIXME: Remove - this is for testing only
+    console.error(`Within Supervisor Monitor Panel syncSubscribed = ${this.#syncSubscribed}`);
 
-        // Set Supervisor's name that is coaching into props
-        this.props.setBargeCoachStatus({ 
-          supervisorArray: supervisorArray
-        });
-      })
-    });
+    if (this.#syncSubscribed != true) {
+      this.#syncSubscribed = true;
+
+      // If the supervisor array has value in it, that means someone is coaching
+      // We will map each of the supervisors that may be actively coaching
+      // Otherwise we will not display anything if no one is actively coaching
+      const agentWorkerSID = this.props.agentWorkerSID;
+      let supervisorArray = this.props.supervisorArray;   
+      // Let's subscribe to the sync doc as an agent/work and check
+      // if we are being coached, if we are, render that in the UI
+      // otherwise leave it blank
+      const mySyncDoc = `syncDoc.${agentWorkerSID}`;
+      SyncDoc.getSyncDoc(mySyncDoc)
+      .then(doc => {
+        // We are subscribing to Sync Doc updates here and logging anytime that happens
+        doc.on("updated", updatedDoc => {
+          if (doc.value.data.supervisors != null) {
+            supervisorArray = [...doc.value.data.supervisors];
+          } else {
+            supervisorArray = [];
+          }
+
+          // Set Supervisor's name that is coaching into props
+          this.props.setBargeCoachStatus({ 
+            supervisorArray: supervisorArray
+          });
+        })
+      });
+    }
+    
+   return;
+ }
+
+  render() {
+
+    this.syncUpdates();
 
     if (this.props.supervisorArray.length != 0) {
       return (
